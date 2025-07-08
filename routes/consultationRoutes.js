9a973c921818c648003544e24ef33f7c4560395f
Shreyas <shreyaskalate9@gmail.com>
2025-07-08 17:25:00 +0530

    Add(consultation): Completed consultation submission logic, getApp returns patient data

--- routes/consultationRoutes.js
+++ routes/consultationRoutes.js
@@ -2,12 +2,12 @@ import express from 'express';
 
 import { authorizeRoles } from '../middleware/roleMiddleware.js';
 import { requireHospitalContext } from '../middleware/hospitalContext.js';
-import { createConsultation, getConsultationByAppointment,getPatientConsultationHistory,getMostCommonDiagnoses } from '../controllers/consultationController.js';
+import { submitConsultation, getConsultationByAppointment,getPatientConsultationHistory,getMostCommonDiagnoses } from '../controllers/consultationController.js';
 
 const router = express.Router();
 router.use(requireHospitalContext);
 
-router.post('/createConsultation', authorizeRoles('doctor'), createConsultation);
+router.post('/submitConsultation', authorizeRoles('doctor'), submitConsultation);
 router.get('/getConsultationByAppointment/:appointmentId', authorizeRoles('doctor', 'receptionist', 'hospitalAdmin'), getConsultationByAppointment);
 router.get('/getPatientConsultationHistory/:patientId', authorizeRoles('doctor', 'hospitalAdmin'), getPatientConsultationHistory);
 router.get('/diagnosis/most-common', authorizeRoles('hospitalAdmin', 'doctor', 'receptionist'), getMostCommonDiagnoses);
