0e2d256ead452e28a1bf720394bb04baa159049e
Akshaya Singh <akshaysing975@gmail.com>
2025-07-16 21:00:48 +0530

    feat:created APIs for get appointments by patient id and patient details by id

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -4,12 +4,21 @@ import { authorizeRoles } from "../middleware/roleMiddleware.js";
 import { requireHospitalContext } from '../middleware/hospitalContext.js';
 import { paginationMiddleware } from '../middleware/paginationMiddleware.js';
 
-import { getPatientsByHospital, getPatientsByStatus  } from "../controllers/patientController.js"
+import { getPatientsByHospital, getPatientsByStatus,getAppointmentsByPatientId, getPatientDetailsById  } from "../controllers/patientController.js"
 
 const router = express.Router()
 router.use(requireHospitalContext);
 
 router.get("/getPatientsByHospital", authorizeRoles("receptionist", "hospitalAdmin"), getPatientsByHospital,paginationMiddleware)
 router.get("/getPatientsByStatus", authorizeRoles("receptionist", "hospitalAdmin"), getPatientsByStatus ,paginationMiddleware);
-
+router.get(
+  "/:patientId/appointments",
+  authorizeRoles("doctor", "receptionist", "hospitalAdmin"),
+  getAppointmentsByPatientId
+);
+router.get(
+  "/:patientId/details",
+  authorizeRoles("receptionist", "hospitalAdmin", "doctor"),
+  getPatientDetailsById
+);
 export default router;
