20e10d748cd2690499d0c5c4c1e6517534f95414
Shreyas <shreyaskalate9@gmail.com>
2025-07-30 10:29:34 +0530

    Update:- Progress tracker now returns valid status

--- routes/consultationRoutes.js
+++ routes/consultationRoutes.js
@@ -9,7 +9,7 @@ router.use(requireHospitalContext);
 
 router.post('/submitConsultation', authorizeRoles('doctor'), submitConsultation);
 router.post('/addProgressPhase', authorizeRoles('doctor'), addProgressPhase);
-router.get('/getProgressTracker/:patientId', authorizeRoles('doctor'), getProgressTracker);
+router.get('/getProgressTracker/:patientId/:caseId', authorizeRoles('doctor'), getProgressTracker);
 router.get('/getProgressPhaseCounts', authorizeRoles('doctor'), getProgressPhaseCounts);
 router.get('/getConsultationByAppointment/:appointmentId', authorizeRoles('doctor', 'receptionist', 'hospitalAdmin'), getConsultationByAppointment);
 router.get('/getPatientConsultationHistory/:patientId', authorizeRoles('doctor', 'hospitalAdmin'), getPatientConsultationHistory);
